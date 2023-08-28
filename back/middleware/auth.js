e7c2f071044bf3fde86e1f94aae1cefa182121b1
ranim-taboush <Trendlix@Thinkpad-1>
2023-08-28 16:37:18 +0300

    ok

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 
 const auth = (req, res, next) => {
   try {
-    const token = req.headers?.accessToken 
+    const token = req.headers?.accessToken || req.headers?.accesstoken 
     if (!req.headers?.accessToken) return res.status(401).json(req?.headers); 
     
     if (!token) return res.status(401).send("You are not authenticated!");
