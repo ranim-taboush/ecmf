4d56bfafba744e8686fe51061414f7e4f09392d6
ranim-taboush <Trendlix@Thinkpad-1>
2023-12-04 17:41:19 +0200

    gallery again

--- client/src/utils/nav-links.ts
+++ client/src/utils/nav-links.ts
@@ -1,7 +1,7 @@
 export const navLinks = [
   { href: '/', label: 'home' },
   { href: '/about', label: 'about' },
-  // { href: '/gallery', label: 'gallery' },
+  { href: '/gallery', label: 'gallery' },
   { href: '/contact-us', label: 'contactUs' },
   { href: '/lines', label: 'lines' },
   { href: '/0/products', label: 'products' },
